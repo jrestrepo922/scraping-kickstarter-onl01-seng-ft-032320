52f627145169696c31116d7ce5db7163d4cffc8f
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-10 13:31:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,7 @@ def create_project_hash
   #iterated through the projects
   kickstarter.css("li.project.grid_4").each { |project|
     title = project.css("h2.bbcard_name strong a").text
-    project[title.to_sym] = {
+    projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
