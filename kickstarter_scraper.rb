716d509badd44f9b369733507e21c9f978e75663
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-10 13:23:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  #binding.pry
+
 
   projects = {}
 
@@ -19,6 +19,7 @@ def create_project_hash
 
   #return the projects hash
   projects
+  #binding.pry
 end
 
 # projects: kickstarter.css("li.project.grid_4")
