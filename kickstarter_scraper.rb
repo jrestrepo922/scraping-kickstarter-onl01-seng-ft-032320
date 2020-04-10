ac8a50f0560c584e9f4d60854a7fa3e5c25de8c9
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-10 12:10:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
+# => no need for Open-Uri since is not a webpage.
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 end
